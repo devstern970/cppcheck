devstern970/cppcheck

commit 58a9e05697b4585883bbc00153d1584d9247190f

Fixed #696 (Tokenizer: Simplify array declaration)
http://sourceforge.net/apps/trac/cppcheck/ticket/696

src/tokenize.cpp
@@ -2502,10 +2502,18 @@ void Tokenizer::simplifyVarDecl()
             }
         }
 
-        else if (Token::Match(tok2, "%type% %var% [ %num% ] ,") ||
-                 Token::Match(tok2, "%type% %var% [ %var% ] ,"))
+        else if (Token::Match(tok2, "%type% %var% [ %num% ] ,|=") ||
+                 Token::Match(tok2, "%type% %var% [ %var% ] ,|="))
         {
             tok2 = tok2->tokAt(5);    // The ',' token
+
+            if (tok2->str() == "=")
+            {
+                while (tok2 && tok2->str() != ",")
+                {
+                    tok2 = tok2->next();
+                }
+            }
         }
 
         else if (Token::Match(tok2, "%type% * %var% [ %num% ] ,") ||

test/testtokenize.cpp
@@ -139,6 +139,7 @@ private:
         TEST_CASE(vardecl5);
         TEST_CASE(vardecl6);
         TEST_CASE(vardecl7);
+        TEST_CASE(vardecl8);
         TEST_CASE(vardecl_stl);
         TEST_CASE(volatile_variables);
         TEST_CASE(syntax_error);
@@ -2236,6 +2237,14 @@ private:
         ASSERT_EQUALS(res, tokenizeAndStringify(code));
     }
 
+    void vardecl8()
+    {
+        // ticket #696
+        const char code[] = "char a[10]={'\\0'}, b[10]={'\\0'};";
+        const char res[]  = "char a [ 10 ] = { '\\0' } ; char b [ 10 ] = { '\\0' } ;";
+
+        ASSERT_EQUALS(res, tokenizeAndStringify(code));
+    }
 
     void volatile_variables()
     {

commit 198017f3c072768d6bd4dbbe99dd6284ed030ae4

Fixed #732 (Tokenizer: Incorrect simplification)
Regression since 58a9e05697b4585883bbc00153d1584d9247190f commit.

http://sourceforge.net/apps/trac/cppcheck/ticket/732

src/tokenize.cpp
@@ -2570,7 +2570,11 @@ void Tokenizer::simplifyVarDecl()
                 {
                     if (tok2->str() == "{")
                         tok2 = tok2->link();
+
                     tok2 = tok2->next();
+
+                    if (tok2->str() == ";")
+                        tok2 = NULL;
                 }
             }
         }

test/testtokenize.cpp
@@ -142,6 +142,7 @@ private:
         TEST_CASE(vardecl7);
         TEST_CASE(vardecl8);
         TEST_CASE(vardecl9);
+        TEST_CASE(vardecl10);
         TEST_CASE(vardecl_stl);
         TEST_CASE(volatile_variables);
         TEST_CASE(syntax_error);
@@ -2287,6 +2288,14 @@ private:
         const char res[]  = "char a [ 2 ] = { 'A' , '\\0' } ; char b [ 2 ] = { 'B' , '\\0' } ;";
 
         ASSERT_EQUALS(res, tokenizeAndStringify(code));
+    }
+
+    void vardecl10()
+    {
+        // ticket #732
+        const char code[] = "char a [ 2 ] = { '-' } ; memset ( a , '-' , sizeof ( a ) ) ;";
+
+        ASSERT_EQUALS(code, tokenizeAndStringify(code));
     }
 
     void volatile_variables()