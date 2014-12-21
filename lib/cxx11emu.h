devstern970/cppcheck

commit fe468ac14255f079cd4ad0546d3c51f8e3f8031a

cppcheck compilation fails with g++4.6.3. Patch from mavik1

lib/cxx11emu.h
@@ -24,7 +24,7 @@
 /* Emulate certain features of C++11 in a C++98-compatible way. */
 
 #ifdef __cplusplus
-#if __cplusplus < 201103L
+#if !defined(__GXX_EXPERIMENTAL_CXX0X__) && __cplusplus < 201103L
 
 // Null pointer literal
 // Source: SC22/WG21/N2431 = J16/07-0301

commit a4de06856b6244c447f120ff124c2fcdfc2d3f2c

Fix compilation with GCC 4.4.
It was broken by fe468ac14255f079cd4ad0546d3c51f8e3f8031a.

lib/cxx11emu.h
@@ -24,7 +24,7 @@
 /* Emulate certain features of C++11 in a C++98-compatible way. */
 
 #ifdef __cplusplus
-#if !defined(__GXX_EXPERIMENTAL_CXX0X__) && __cplusplus < 201103L
+#if (__GNUC__ <= 4 && __GNUC_MINOR__ < 6) || __cplusplus < 201103L
 
 // Null pointer literal
 // Source: SC22/WG21/N2431 = J16/07-0301